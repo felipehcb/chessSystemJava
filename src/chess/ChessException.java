3de7d08a7cd3358f1ce09c03032b85ee2b9e5c44
felipehcb <felipehcb14@gmail.com>
2024-06-27 21:03:08 -0300

    Moving pieces

--- src/chess/ChessException.java
+++ src/chess/ChessException.java
@@ -1,9 +1,11 @@
 package chess;
 
-public class ChessException extends RuntimeException{
-    private static final long serialVersionUID= 1L;
+import boardgame.BoardException;
 
-    public ChessException(String msg){
-        super (msg);
+public class ChessException extends BoardException {
+    private static final long serialVersionUID = 1L;
+
+    public ChessException(String msg) {
+        super(msg);
     }
 }
